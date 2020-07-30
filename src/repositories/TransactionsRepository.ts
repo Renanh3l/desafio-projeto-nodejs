e82ade0234edf659292217abe1583663f2cb9b1c
Renan Henrique <nan.pp@hotmail.com>
2020-07-29 21:10:39 -0300

    Conclui o desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,19 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface ListTransactionDTO {
+  transactions: Transaction[];
+  balance: Balance;
 }
 
 class TransactionsRepository {
@@ -13,16 +23,73 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): ListTransactionDTO {
+    const balance = this.getBalance();
+    const { transactions } = this;
+
+    const list = {
+      transactions,
+      balance,
+    };
+
+    return list;
   }
 
   public getBalance(): Balance {
-    // TODO
+    if (this.transactions.length > 0) {
+      const balanceIncome = this.transactions
+        .filter(t => t.type === 'income')
+        .reduce(
+          (previous, current) => {
+            return new Transaction({
+              title: previous.title,
+              value: previous.value + current.value,
+              type: 'income',
+            });
+          },
+          {
+            title: '',
+            value: 0,
+            type: 'income',
+          },
+        );
+
+      const balanceOutcome = this.transactions
+        .filter(t => t.type === 'outcome')
+        .reduce(
+          (previous, current) => {
+            return new Transaction({
+              title: previous.title,
+              value: previous.value + current.value,
+              type: 'outcome',
+            });
+          },
+          {
+            title: '',
+            value: 0,
+            type: 'outcome',
+          },
+        );
+
+      const balanceTotal = balanceIncome.value - balanceOutcome.value;
+
+      const balance = {
+        income: balanceIncome.value,
+        outcome: balanceOutcome.value,
+        total: balanceTotal,
+      };
+
+      return balance;
+    }
+    return { income: 0, outcome: 0, total: 0 };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
